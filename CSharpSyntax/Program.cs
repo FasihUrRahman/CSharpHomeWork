be92a6bd716fbb50e4851597773db64eb1ff40d1
Fasih Ur Rahman <fasihurrahmandeveloper@gmail.com>
2023-04-17 19:23:15 +0500

    Upload 17 Apr 2023

--- CSharpSyntax/Program.cs
+++ CSharpSyntax/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿Console.WriteLine("\t\tThis is Practice of Syntax and Comment");//Console.Write("")->it's use for printe something on screen.
+Console.WriteLine("Hello Everyone!");//Hello syntax 
+Console.WriteLine("This is Some Introduction About Me."); //Heading
+Console.WriteLine("Let's Start With This.");//Startup Line
+Console.Write("\tMy Name is Fasih Ur Rahman and i am 25 Years Old. My Date of Birth is 25 June 1998. \n\tI Worked as a Freelance Photographer.");//About Me
+Console.ReadKey();//Console.ReadKey()-> This Function is used for getting input.
